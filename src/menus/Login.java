ee0f6ebf81674dade324c8ad96fe8cb10152db9b
Asus <an.lyubenov@abv.bg>
2022-11-23 20:31:36 +0200

    adding methods getUserChoice(), choiceOptionLoginMenu() and loginMenu in class Login; adding methods promptEnterKey in class PressEnter

--- src/menus/Login.java
+++ src/menus/Login.java
@@ -1,6 +1,38 @@
 package menus;
 
+import services.Validators;
+
+import java.util.Scanner;
+
 
 public class Login {
 
+
+
+
+
+    public static String getUserChoice(){
+        System.out.println("  ===== A & A =====   ");
+        System.out.println("    = Solutions =    ");
+        System.out.println("   == Login Menu ==    ");
+        System.out.println();
+        System.out.println("1. Login.");
+        System.out.println("0. EXIT.");
+        System.out.print("Your choice - > ");
+        Scanner sc = new Scanner(System.in);
+        return sc.next();
+    }
+    private static void chooseOptionLoginMenu() {
+        int choice = Validators.validateChoice(getUserChoice(), 1);
+        if (choice == -1) {
+            chooseOptionLoginMenu();
+        } else if (choice == 1) {
+
+        }
+    }
+
+    public static void loginMenu() {
+        chooseOptionLoginMenu();
+
+    }
 }
