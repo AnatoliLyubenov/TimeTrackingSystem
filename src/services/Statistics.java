3b098350e69e3dce923be9fc20a0b9210db8dc2a
Asus <an.lyubenov@abv.bg>
2022-11-24 20:56:18 +0200

    I adding methods into class CreateObj, class Statistics,class StatisticsMenu and ValidateDate.

--- src/services/Statistics.java
+++ src/services/Statistics.java
@@ -1,7 +1,25 @@
 package services;
 
 
+import models.Account;
+
+import java.util.HashMap;
+
+import static services.PrintOut.showEmployeesList;
 
 public class Statistics {
+
+    public static void searchByEmployeeName() {
+        HashMap<String, Account> accountsList = Load.accountsListFromFile();
+        showEmployeesList(accountsList);
+        String accountName = getEmployeeName();
+        if (ValidateRepeatingEmployee.checkRepeatingEmployee(accountName)) {
+            showEmployeeStatistics(accountsList, accountName);
+        } else {
+            System.out.println("Non-existent Employee Account Name!\nCheck the list closely!");
+            PressEnter.promptEnterKey();
+            searchByEmployeeName();
+        }
+    }
 
 }
