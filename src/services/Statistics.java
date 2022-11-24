acffd37ad45a7bf93a6fbf58fdb032dcf4ff98df
Asus <an.lyubenov@abv.bg>
2022-11-24 21:49:35 +0200

    ...and method into class Statistics...

--- src/services/Statistics.java
+++ src/services/Statistics.java
@@ -1,11 +1,13 @@
 package services;
 
 
+import menus.AdminMenu;
 import models.Account;
 
 import java.util.HashMap;
 import java.util.Scanner;
 
+import static services.PrintOut.showEmployeeClients;
 import static services.PrintOut.showEmployeesList;
 
 public class Statistics {
@@ -15,6 +17,28 @@ public class Statistics {
         Scanner sc = new Scanner(System.in);
         return sc.nextLine().toUpperCase();
     }
+    public static void showEmployeeStatistics(HashMap<String, Account> accountsList, String accountName) {
+        for (String str : accountsList.keySet()) {
+            if (str.equalsIgnoreCase(accountName)) {
+                try {
+                    if (accountsList.get(str).getDailyProtocols() == null) {
+                        System.out.println(accountName + "'s Protocol List is empty.");
+                    } else {
+                        System.out.println();
+                        System.out.println("=========== " + accountsList.get(accountName).getName() + " Protocol List ===========");
+                        showEmployeeClients(accountName);
+                    }
+                    PressEnter.promptEnterKey();
+                    AdminMenu.menu();
+                } catch (NullPointerException e) {
+                    System.out.println(accountName + "'s Protocol List is empty.");
+                    PressEnter.promptEnterKey();
+                    AdminMenu.menu();
+                }
+            }
+        }
+    }
+
 
     public static void searchByEmployeeName() {
         HashMap<String, Account> accountsList = Load.accountsListFromFile();
