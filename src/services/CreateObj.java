e4402a1efaaa991bdd343d671caa0e4fcfa32b0d
Asus <an.lyubenov@abv.bg>
2022-11-25 06:01:18 +0200

    I tried to fix one method into CreateObj and adding new method into class PrintOut

--- src/services/CreateObj.java
+++ src/services/CreateObj.java
@@ -8,30 +8,25 @@ import models.Employee;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+public class CreateObj {
+    public static int adminObj(String accountName,String accountPassword, String filePath) {
+        HashMap<String, Account> accountsList = new HashMap<>();
+        accountsList.put(accountName, new Admin(accountName, accountPassword, true));
+        Save.accountsListToFile(accountsList,filePath);
+        return 1;
+    }
 
-    public class CreateObj {
-        public static int adminObj(String accountName, String accountPassword, String filePath) {
-            HashMap<String, Account> accountsList = new HashMap<>();
-            accountsList.put(accountName, new Admin(accountName, accountPassword, true));
-            Save.accountsListToFile(accountsList, filePath);
-            return 1;
-
-        }
-        public static int employeeObj(String accountName, String accountPassword, String name,String filePath) {
-            HashMap<String, Account> accountsList = Load.accountsListFromFile();
-            accountsList.put(accountName, new Employee(accountName, accountPassword, name, false));
-            Save.accountsListToFile(accountsList,filePath);
-            System.out.println("Employee Account "+accountName+" successfully created.");
-            return 1;
-        }
-
-        public static int clientObj(String clientName, String projectName, String date,String filePath) {
-            ArrayList<Client> clientsList = Load.clientListFromFile();
-            clientsList.add(new Client(clientName, projectName, date));
-            Save.clientListToFile(clientsList, filePath);// "ProgramFiles/ClientsList.txt"
-            return 1;
-        }
+    public static int employeeObj(String accountName, String accountPassword, String name,String filePath) {
+        HashMap<String, Account> accountsList = Load.accountsListFromFile();
+        accountsList.put(accountName, new Employee(accountName, accountPassword, name, false));
+        Save.accountsListToFile(accountsList,filePath);
+        System.out.println("Employee Account "+accountName+" successfully created.");
+        return 1;
+    }
+    public static int clientObj(String clientName, String projectName, String date,String filePath) {
+        ArrayList<Client> clientsList=Load.clientListFromFile();
+        clientsList.add(new Client(clientName, projectName, date));
+        Save.clientListToFile(clientsList,filePath);// "ProgramFiles/ClientsList.txt"
+        return 1;
+    }
 }
-
-
-
--- src/services/PrintOut.java
+++ src/services/PrintOut.java
@@ -1,7 +1,9 @@
 package services;
 
 import models.Account;
+import models.Protocol;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PrintOut {
@@ -15,6 +17,28 @@ public class PrintOut {
                 System.out.println("Employee Name - > " + accountsList.get(str).getName());
                 System.out.println("================================================================");
             }
+        }
+    }
+    public static void showEmployeeClients(String accountName) {
+        HashMap<String, Account> accountsList = Load.accountsListFromFile();
+        HashMap<String, ArrayList<Protocol>> employeeProtocols = accountsList.get(accountName).getDailyProtocols();
+        int totalDailyWorkTime = 0;
+
+        for (String date : employeeProtocols.keySet()) {
+            ArrayList<Protocol> employeeDailyProtocols = employeeProtocols.get(date);
+            for (int i = 0; i < employeeDailyProtocols.size(); i++) {
+                System.out.println("Client Name - > " + employeeDailyProtocols.get(i).getClientName());
+                System.out.println("Project Name - > " + employeeDailyProtocols.get(i).getProject());
+                System.out.println("Expiration Date - > " + employeeDailyProtocols.get(i).getDeadLine());
+                System.out.println("Work time spend on " + date + " - > " + CalculateWorkTimeH.convertMtoH(employeeDailyProtocols.get(i).getMinutes()));
+                System.out.println("================================================================");
+                totalDailyWorkTime += employeeDailyProtocols.get(i).getMinutes();
+            }
+            System.out.println("*******************************************************************************");
+            System.out.println(date + " - " + accountsList.get(accountName).getName() + " work time spend on Clients.txt - > " + CalculateWorkTimeH.convertMtoH(totalDailyWorkTime));
+            System.out.println("*******************************************************************************");
+            System.out.println();
+            totalDailyWorkTime = 0;//reset work time for the next date
         }
     }
 
