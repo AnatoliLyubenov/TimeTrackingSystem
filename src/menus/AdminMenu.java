05489f986166ed17635a99f49529809d0983e439
Asus <an.lyubenov@abv.bg>
2022-11-26 10:55:10 +0200

    edit method

--- src/menus/AdminMenu.java
+++ src/menus/AdminMenu.java
@@ -104,22 +104,7 @@ public class AdminMenu {
         }
         AdminMenu.menu();
     }
-    public static boolean validateEnterClientFieldsAreNotEmpty(String clientName,String projectName,String expirationDate) {
-        boolean isNotEmpty = true;
-        HashMap<String, String> fields = new HashMap<String, String>();
-        fields.put(clientName, "\"Client's Name\"");
-        fields.put(projectName, "\"Project's Name\"");
-        fields.put(expirationDate, "\"Expiration date\"");
 
-        for (String field : fields.keySet()) {
-            if (field.equals("")) {
-                System.out.println("Empty fields are NOT acceptable!!!");
-                System.out.println(fields.get(field) + " field can NOT be empty!");
-                isNotEmpty = false;
-            }
-        }
-        return isNotEmpty;
-    }
 
     public static void menu() {
         chooseAdminMenuOption();
