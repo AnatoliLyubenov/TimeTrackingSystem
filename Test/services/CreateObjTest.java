f3d10f6bc259987d10c7f4aee47e345d1af9ed1b
Asus <an.lyubenov@abv.bg>
2022-11-27 13:39:48 +0200

    create test Client Object

--- Test/services/CreateObjTest.java
+++ Test/services/CreateObjTest.java
@@ -10,14 +10,14 @@ public class CreateObjTest {
     void adminObjCreated() {
         String accountName = "accountName";
         String password = "password";
-        assertEquals(1,CreateObj.adminObj(accountName,password,"AccountsList.txt"));
+        assertEquals(1, CreateObj.adminObj(accountName, password, "AccountsList.txt"));
     }
 
     @Test
     void adminObjNotCreated() {
         String accountName = "accountName";
         String password = "password";
-        assertThrows(RuntimeException.class,()->CreateObj.adminObj(accountName,password,"//"));
+        assertThrows(RuntimeException.class, () -> CreateObj.adminObj(accountName, password, "//"));
     }
 
     @Test
@@ -25,15 +25,31 @@ public class CreateObjTest {
         String accountName = "accountName";
         String password = "password";
         String name = "Anton";
-        assertEquals(1,CreateObj.employeeObj(accountName,password,name,"AccountsList.txt"));
+        assertEquals(1, CreateObj.employeeObj(accountName, password, name, "AccountsList.txt"));
     }
+
     @Test
     void employeeObjNotCreated() {
         String accountName = "accountName";
         String password = "password";
         String name = "Anton";
-        assertThrows(RuntimeException.class,()->CreateObj.employeeObj(accountName,password,name,"//"));
+        assertThrows(RuntimeException.class, () -> CreateObj.employeeObj(accountName, password, name, "//"));
     }
 
+    @Test
+    void clientObjectCreated() {
+        String clientName = "clientName";
+        String projectName = "test project";
+        String date = "31.12.2022";
+        assertEquals(1, CreateObj.clientObj(clientName, projectName, date, "ClientsListTest.txt"));
+    }
 
+    @Test
+    void clientObjNotCreated() {
+        String clientName = "clientName";
+        String projectName = "test project";
+        String date = "31.12.2022";
+        assertThrows(RuntimeException.class, () -> CreateObj.clientObj(clientName, projectName, date, "//"));
+    }
 }
+
